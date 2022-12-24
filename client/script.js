07987867997007d68b96f3feb34c9bb1070d3cc9
Dev <pateldev2808@gmail.com>
2022-12-25 00:49:08 +0530

    update /

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async e => {
 
   loader(messageDiv);
 
-  const response = await fetch('https://devx.onrender.com/', {
+  const response = await fetch('https://devx.onrender.com', {
     method: 'POST',
     headers: {
       'Content-type': 'application/json'
--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ import { Configuration, OpenAIApi } from 'openai'
 dotenv.config();
 
 
-
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
