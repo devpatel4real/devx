cdb653d79b34401d4976c2b7f956113ca8749713
Dev <pateldev2808@gmail.com>
2022-12-24 23:49:24 +0530

    update

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async e => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:9000', {
+  const response = await fetch('https://devx.onrender.com/', {
     method: 'POST',
     headers: {
       'content-type': 'application/json'
